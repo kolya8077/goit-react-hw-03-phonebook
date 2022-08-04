a90e19c9fa08a39fae8cad98d6571e6052afb937
kolya8077 <kolya8077@gmail.com>
2022-08-04 22:09:59 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -58,6 +58,19 @@ class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts)
+
+    this.setState({contacts: parsedContacts})
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
 
